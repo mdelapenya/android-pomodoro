8aac24aef47ccdfe73f7903b30d03f8348f69c6e
Manuel de la Peña <manuel.delapenya@liferay.com>
2017-03-03 11:50:53 +0100

    Initialize widgets onCreate

--- app/src/main/java/es/mdelapenya/android/pomodoro/MainActivity.java
+++ app/src/main/java/es/mdelapenya/android/pomodoro/MainActivity.java
@@ -2,6 +2,8 @@ package es.mdelapenya.android.pomodoro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,11 +14,19 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        btnPause = (Button) findViewById(R.id.btnPause);
+        btnStart = (Button) findViewById(R.id.btnStart);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
         txtPomodoro = (TextView) findViewById(R.id.pomodoroTime);
 
+        progressBar.setMax(60);
+        progressBar.setProgress(0);
         txtPomodoro.setText(R.string.defaultPomodoroValue);
     }
 
+    private Button btnPause;
+    private Button btnStart;
+    private ProgressBar progressBar;
     private TextView txtPomodoro;
 
 }
