0d0b7e12071b868a060a62d51836dfc3385ce73d
Manuel de la Peña <manuel.delapenya@liferay.com>
2017-03-03 11:44:06 +0100

    Initialize pomodoro text

--- app/src/main/java/es/mdelapenya/android/pomodoro/MainActivity.java
+++ app/src/main/java/es/mdelapenya/android/pomodoro/MainActivity.java
@@ -2,12 +2,21 @@ package es.mdelapenya.android.pomodoro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        txtPomodoro = (TextView) findViewById(R.id.pomodoroTime);
+
+        txtPomodoro.setText(R.string.defaultPomodoroValue);
     }
+
+    private TextView txtPomodoro;
+
 }
